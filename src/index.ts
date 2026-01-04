e82988f81cdad45647606749ea5d1e4e21263f5c
Bhim magar <shokokomichan098@gmail.com>
2026-01-04 19:23:21 +0545

    commented out the entire code

--- src/index.ts
+++ src/index.ts
@@ -9,67 +9,68 @@
 */
 
 // any
-let anyVar: any = "Hello";
-anyVar = 5;
-anyVar = true;
-console.log(anyVar)
+// let anyVar: any = "Hello";
+// anyVar = 5;
+// anyVar = true;
+// console.log(anyVar)
 
-// unknown
-let unknownVar: unknown = "Hello";
-if (typeof unknownVar === "string") {
-  console.log(unknownVar.toUpperCase());
-}
+// // unknown
+// let unknownVar: unknown = "Hello";
+// if (typeof unknownVar === "string") {
+//   console.log(unknownVar.toUpperCase());
+// }
 
-// enum
-enum Color {
-  Red,
-  Green,
-  Blue,
-}
-let c: Color = Color.Green;
-console.log(c);
+// // enum
+// enum Color {
+//   Red,
+//   Green,
+//   Blue,
+// }
+// let c: Color = Color.Green;
+// console.log(c);
 
-// never
-function error(message: string): never {
-  throw new Error(message);
-}
+// // never
+// function error(message: string): never {
+//   throw new Error(message);
+// }
 
-console.log(error("This is an error message."));
+// console.log(error("This is an error message."));
 
-// tuple
-let tuple: [string, number] = ["hello", 10];
-console.log(tuple);
+// // tuple
+// let tuple: [string, number] = ["hello", 10];
+// console.log(tuple);
 
-// function
-const add = (a: number, b: number): number => {
-    return a + b;
-}
-console.log(add(1, 2));
-
-
-// array
-let arr: number[] = [1, 2, 3, 4, 5];
-console.log(arr);
+// // function
+// const add = (a: number, b: number): number => {
+//     return a + b;
+// }
+// console.log(add(1, 2));
 
 
-// object
-let obj: { name: string; age: number } = {
-    name: "John",
-    age: 30
-}
-console.log(obj);
+// // array
+// let arr: number[] = [1, 2, 3, 4, 5];
+// console.log(arr);
 
 
-type Vim = {
-  name: string;
-  id: number;
-  isActive: boolean;
-};
+// // object
+// let obj: { name: string; age: number } = {
+//     name: "John",
+//     age: 30
+// }
+// console.log(obj);
 
-let sayHello: Vim = {
-  name: "vim",
-  id: 1,
-  isActive: true,
-};
 
-console.log(sayHello);
+// type Vim = {
+//   name: string;
+//   id: number;
+//   isActive: boolean;
+// };
+
+// let sayHello: Vim = {
+//   name: "vim",
+//   id: 1,
+//   isActive: true,
+// };
+
+// console.log(sayHello);
+
